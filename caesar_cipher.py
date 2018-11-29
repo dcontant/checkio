7208e06a34f8b30bab1a7dea829e6272aa510010
denys <denyscontant@gmail.com>
2018-11-29 15:16:30 -0500

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,14 @@
+from string import ascii_lowercase as letters
+
+def to_encrypt(text, n):
+    return text.translate(str.maketrans(letters, letters[n:]+letters[:n]))
+
+
+if __name__ == '__main__':
+    
+    assert to_encrypt("a b c", 3) == "d e f"
+    assert to_encrypt("a b c", -3) == "x y z"
+    assert to_encrypt("simple text", 16) == "iycfbu junj"
+    assert to_encrypt("important text", 10) == "swzybdkxd dohd"
+    assert to_encrypt("state secret", -13) == "fgngr frperg"
+    print("all good")
