be7e967cea571051f444ada96ef929e82f15445d
denys <denyscontant@gmail.com>
2018-12-16 20:01:49 -0500

    Create Moore neighborhood.py

---
+++ Moore neighborhood.py
@@ -0,0 +1,29 @@
+def count_neighbours(grid, row, col):
+    ans = 0
+    imax = len(grid)
+    jmax = len(grid[0])
+    for i in range(row-1,row+2):
+        for j in range(col-1,col+2):
+            if 0<=i and 0<=j and i<imax and j<jmax and not (i==row and j==col):
+                ans += grid[i][j]
+    return ans
+
+
+if __name__ == '__main__':
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
+    print('all good')
